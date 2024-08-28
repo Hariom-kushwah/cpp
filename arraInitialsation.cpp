ecb3a87073baa4e0d0a149b25e20b1b9c1badcf3
Hariom-kushwah <121570066+Hariom-kushwah@users.noreply.github.com>
2024-08-28 22:34:29 +0530

    Add files via upload

---
+++ arraInitialsation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int arr[]={1,2,1,5,3};
+    // int arr[5]={1,2,1,5,3};
+    // int arr[12]={1,2,1,5,3};
+    // error  int arr[3]={1,2,1,5,3};
+   //int arr[5] = {3,5,1,  7, 9,};
+   // int n = 5;
+    // cout<<arr[2]<<endl;
+    // cout<<arr[6];
+    ////for (int i = 0; i < n; i++)
+   // {
+       // cout << arr[i];
+    //}
+
+    //Taking input an array
+
+
+
+    int arr[5];
+    int n;
+    
+}
